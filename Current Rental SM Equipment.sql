dc261c7e7b2187e3f3878a9a198d1b7b44acc64a
cmetzger4 <cmetzger4@gmail.com>
2019-02-12 11:11:07 -0500

    Rental SM: Addition of Invoice Date The rental department (LH) requested that the invoice date of the last SM work order be included on their report. To pull these dates in, Table 'wohead' needed to be brought in to get the invoice number and then that number is used to locate the date in Table 'ardetl'.

--- Current Rental SM Equipment.sql
+++ Current Rental SM Equipment.sql
@@ -1,16 +1,25 @@
-SELECT kmfg       AS [MANUFACTURER]
-     , kequipnum  AS [EQUIPMENT ID]
-     , kmodel     AS [MODEL]
-     , kserialno1 AS [SERIAL NUMBER]
-     , eqmkready  AS [CODE]
-     , int_flag_1 AS [FREQUENCY]
-     , kbranch    AS [BRANCH]
-     , kworkorder AS [LAST WORK ORDER NUMBER]
-     , datelstbl  AS [LAST SM DATE]
-     , lasthours  AS [LAST HOUR METER READING]
-     , datenxtbl  AS [NEXT SM DATE]
-     , pmsell     AS [FIXED RATE]
-FROM eqmnt                                      /* SM Master Table   */
-WHERE kcustnum IN ('RENT100'                    /* Cincinnati Rental */
-                 , 'RENT200'                    /* Columbus Rental   */
-                 , 'RENT300')                   /* Louisville Rental */
+SELECT eqmnt.kmfg       AS [MANUFACTURER]
+     , eqmnt.kequipnum  AS [EQUIPMENT ID]
+     , eqmnt.kmodel     AS [MODEL]
+     , eqmnt.kserialno1 AS [SERIAL NUMBER]
+     , eqmnt.eqmkready  AS [LABOR CODE]
+     , eqmnt.int_flag_1 AS [FREQUENCY]
+     , eqmnt.kbranch    AS [BRANCH]
+     , eqmnt.kworkorder AS [LAST WORK ORDER NUMBER]
+     , eqmnt.datelstbl  AS [LAST SM DATE]
+     , eqmnt.lasthours  AS [LAST HOUR METER READING]
+     , eqmnt.datenxtbl  AS [NEXT SM DATE]
+     , eqmnt.pmsell     AS [FIXED RATE]
+     , eqmnt.kworkorder AS [LAST PM WORK ORDER NUMBER]
+     , ardetl.ardate    AS [INVOICE DATE]
+FROM eqmnt                                            /* SM Master Table   */
+LEFT OUTER JOIN wohead                                /* Work Order Header Table */
+             ON eqmnt.kworkorder = wohead.kworkorder
+            AND eqmnt.kbranch = wohead.kbranch
+LEFT OUTER JOIN ardetl                                /* Accounts Receivable Detail Table */
+             ON wohead.arinvno = ardetl.arinvno
+            AND wohead.kbranch = ardetl.arbranch
+            AND ardetl.artype = 1                     /* Type: Invoice */
+WHERE eqmnt.kcustnum IN ('RENT100'                    /* Cincinnati Rental */
+                       , 'RENT200'                    /* Columbus Rental   */
+                       , 'RENT300')                   /* Louisville Rental */
